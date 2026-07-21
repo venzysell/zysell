030e531eb9113a9684cec38774e940b76850da40
venzysell <venzysell@gmail.com>
2026-07-21 03:29:33 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,44 @@
 // ==============================
-// ZYSELL APP
-// Version 1.0
+// ZYSELL APP V1.1
 // ==============================
 
+// Splash Screen
 console.log("Bienvenue sur Zysell 🚀");
 
-// Les fonctionnalités seront ajoutées progressivement.
+// Création automatique du bouton menu mobile
+
+const header = document.querySelector(".header .container");
+
+const menuButton = document.createElement("div");
+
+menuButton.className = "menu-toggle";
+
+menuButton.innerHTML = "☰";
+
+header.appendChild(menuButton);
+
+const navbar = document.querySelector(".navbar");
+
+menuButton.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+});
+
+// Effet au défilement
+
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector(".header");
+
+    if(window.scrollY > 40){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
